b634d30ad23d1e1c4d0014219e23d6eb9c94eff0
Bhavesh0577 <114731143+Bhavesh0577@users.noreply.github.com>
2024-08-28 21:57:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '04abc99102mshd12805729c6a2f6p12c173jsn7731d1733b7d',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'YOUR_API_KEY_HERE',
+        'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+    }
 };
-const getWeather = (city) =>{
-	cityName.innerHTML = city
-	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
-	.then(response => response.json())
-	.then((response) => {
-		console.log(response)
-		//cloud_pct.innerHTML = response.cloud_pct
-		feels_like.innerHTML = response.feels_like
-		humidity.innerHTML = response.humidity
-		humidity2.innerHTML = response.humidity
-		max_temp.innerHTML = response.max_temp
-		min_temp.innerHTML = response.min_temp
-		sunrise.innerHTML = response.sunrise
-		sunset.innerHTML = response.sunset
-		temp.innerHTML = response.temp
-		temp2.innerHTML = response.temp
-		wind_degrees.innerHTML = response.wind_degrees
-		wind_speed.innerHTML = response.wind_speed
-		wind_speed2.innerHTML = response.wind_speed
-	})
-	.catch(err => console.error(err));
-}
 
-submit.addEventListener("click", (e)=>{
-	e.preventDefault()
-	getWeather(city.value)
-})
-getWeather("Bengaluru")
+const getWeather = (city) => {
+    cityName.innerHTML = city;
+    fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((response) => {
+            console.log(response);
+            feels_like.innerHTML = response.feels_like || "N/A";
+            humidity.innerHTML = response.humidity || "N/A";
+            humidity2.innerHTML = response.humidity || "N/A";
+            max_temp.innerHTML = response.max_temp || "N/A";
+            min_temp.innerHTML = response.min_temp || "N/A";
+            sunrise.innerHTML = response.sunrise || "N/A";
+            sunset.innerHTML = response.sunset || "N/A";
+            temp.innerHTML = response.temp || "N/A";
+            temp2.innerHTML = response.temp || "N/A";
+            wind_degrees.innerHTML = response.wind_degrees || "N/A";
+            wind_speed.innerHTML = response.wind_speed || "N/A";
+            wind_speed2.innerHTML = response.wind_speed || "N/A";
+        })
+        .catch(err => console.error('Fetch error: ', err));
+};
+
+submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    getWeather(city.value);
+});
+
+getWeather("Bengaluru");
